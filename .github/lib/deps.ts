9a3093cf0a676867843a11c3a1bdaa6c8aedf557
Johan Borestad <johan@borestad.com>
2024-09-19 20:53:37 +0200

    `️️⚡️ SQUASHED ⚡️` `2024/09/19 20:53`

---
+++ .github/lib/deps.ts
@@ -0,0 +1,10 @@
+import _chalk from 'npm:chalk@5'
+
+export { $ } from 'dax/mod.ts'
+export { colors } from 'dax/src/deps.ts'
+export * as uuid from 'std/uuid/mod.ts'
+export * as path from 'std/path/mod.ts'
+export const { log } = console
+export * as P from 'npm:blend-promise-utils'
+export * as dax from 'dax/src/deps.ts'
+export const chalk = _chalk
